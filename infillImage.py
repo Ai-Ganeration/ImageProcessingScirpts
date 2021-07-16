ee2ef79c31cfb94aa8298286b18382805c17be8c
urbanjoe22 <joegxu22@gmail.com>
2021-07-16 10:14:37 -0400

    this version works on colab

--- infillImage.py
+++ infillImage.py
@@ -1,8 +1,21 @@
 import cv2
 import numpy as np
+import os
 
-# Open the image.
-img = cv2.imread('image.png')
+# hack for getting the image name
+f = []
+for (dirpath, dirnames, filenames) in os.walk(input_image_path):
+  path_id.extend(dirnames)
+  f.extend(filenames)
+  break
+
+image_name = f[0]
+print(image_name)
+image_fullpath = os.path.join(input_image_path, image_name)
+print(image_fullpath)
+
+# open the image
+img = cv2.imread(image_fullpath)
 
 width = img.shape[1]
 height = img.shape[0]
@@ -26,8 +39,9 @@ square_image = cv2.copyMakeBorder(
     value = [0,0,0] # black
 )
 
-# write the output.
-cv2.imwrite('image-with-border.png', square_image)
+# write the output
+image_with_border_path = os.path.join(input_image_path, "image-with-border.png")
+cv2.imwrite(image_with_border_path, square_image)
 
 # create mask
 blank_canvas = np.zeros((height, width, 3))
@@ -45,20 +59,27 @@ mask = cv2.copyMakeBorder(
     value = [255,255,255] # black
 )
 
-cv2.imwrite('image-mask.png', mask)
-
+image_mask_path = os.path.join(input_image_path, "image-mask.png")
+cv2.imwrite(image_mask_path, mask)
 
 # open images
-input_image = cv2.imread('image-with-border.png')
-mask_image = cv2.imread('image-mask.png', cv2.IMREAD_GRAYSCALE)
+input_image = cv2.imread(image_with_border_path)
+mask_image = cv2.imread(image_mask_path, cv2.IMREAD_GRAYSCALE)
 
 # in-fill the image
 painted_image = cv2.inpaint(input_image, mask_image, 3, cv2.INPAINT_TELEA) # cv2.INPAINT_NS
-cv2.imwrite('image-painted.png', painted_image)
 
 # resize image
 target_width = 512
 target_height = 512
 target_size = (target_width, target_height)
 resized_image = cv2.resize(painted_image, dsize=target_size, interpolation=cv2.INTER_AREA)
-cv2.imwrite('image-processed.png', resized_image)
+
+# deletes existing images
+dir = input_image_path
+for f in os.listdir(dir):
+    os.remove(os.path.join(dir, f))
+
+# write image
+image_mask_path = os.path.join(input_image_path, "image.png")
+cv2.imwrite(image_mask_path, resized_image)
